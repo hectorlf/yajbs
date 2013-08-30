5d64c8b87823eed6f8a4189f15be427409a4d9e4
hector <drht@lycos.com>
2013-08-30 19:44:47 +0200

    - Eliminado un warning de una variable no usada

--- Blog/src/com/hectorlopezfernandez/action/TagsAction.java
+++ Blog/src/com/hectorlopezfernandez/action/TagsAction.java
@@ -10,9 +10,7 @@ import net.sourceforge.stripes.action.UrlBinding;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.inject.Inject;
 import com.hectorlopezfernandez.integration.BlogActionBeanContext;
-import com.hectorlopezfernandez.service.PostService;
 
 @UrlBinding("/tags/{name}/{overhead}")
 public class TagsAction implements ActionBean {
@@ -20,7 +18,6 @@ public class TagsAction implements ActionBean {
 	private final static Logger logger = LoggerFactory.getLogger(TagsAction.class);
 
 	private BlogActionBeanContext ctx;
-	@Inject private PostService postService;
 	
 	// campos que guarda el actionbean
 	
@@ -62,10 +59,6 @@ public class TagsAction implements ActionBean {
 	@Override
 	public void setContext(ActionBeanContext ctx) {
 		this.ctx = (BlogActionBeanContext)ctx;
-	}
-
-	public void setPostService(PostService postService) {
-		this.postService = postService;
 	}
 
 }
