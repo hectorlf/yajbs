4ebcc2c7d60db54c014c008246fa6878e81082a5
Héctor <hectorlopezfernandez@hotmail.com>
2015-01-03 17:07:05 +0100

    Corregido un error que mostraba mal el UTF-8 en los feeds - No se estaba usando la codificación correcta al parsear el html con nekohtml y se introducían caracteres extraños en los feeds - Esto también afectaba al indexado de lucene, porque comparten el código de parseo

--- Blog/src/com/hectorlopezfernandez/utils/HTMLUtils.java
+++ Blog/src/com/hectorlopezfernandez/utils/HTMLUtils.java
@@ -25,14 +25,17 @@ import org.xml.sax.InputSource;
 
 public final class HTMLUtils {
 
-	private final static Logger logger = LoggerFactory.getLogger(HTMLUtils.class);
+	private static final Logger logger = LoggerFactory.getLogger(HTMLUtils.class);
+
+	private static final String[] HREF_ATTRIBUTE = new String[] { "href" };
+	private static final String[] SRC_ATTRIBUTE = new String[] { "src" };
 
 	// no instanciable
 	private HTMLUtils() {};
 
 	/**
 	 * Elimina todos los tags de html excepto <p>, <a> con su href e <img> con su src.
-	 * Específico para los feeds atom.
+	 * Especï¿½fico para los feeds atom.
 	 */
 	public static String parseTextForFeeds(String text) {
 		logger.debug("Procesando html para presentar en un feed: {}", text);
@@ -40,8 +43,8 @@ public final class HTMLUtils {
 		// se crean los elementos del parseador de nekohtml
 		ElementRemover remover = new ElementRemover();
 		remover.acceptElement("p", null);
-		remover.acceptElement("a", new String[] { "href" });
-		remover.acceptElement("img", new String[] { "src" });
+		remover.acceptElement("a", HREF_ATTRIBUTE);
+		remover.acceptElement("img", SRC_ATTRIBUTE);
 		DOMFragmentParser parser = new DOMFragmentParser();
 		HTMLDocument document = new HTMLDocumentImpl();
 		DocumentFragment fragment = document.createDocumentFragment();
@@ -49,14 +52,15 @@ public final class HTMLUtils {
 			InputStream is = new ByteArrayInputStream(text.getBytes("UTF-8"));
 			InputSource inputSource = new InputSource(is);
 			parser.setProperty("http://cyberneko.org/html/properties/filters", new XMLDocumentFilter[] { remover });
+			parser.setProperty("http://cyberneko.org/html/properties/default-encoding", "UTF-8");
 			parser.parse(inputSource, fragment);
 		} catch(Exception e) {
-			// en el remoto caso de que ocurra una excepción parseando el texto, se devuelve la cadena vacía y a correr
-			// no tiene mucho sentido levantar excepción por esto
+			// en el remoto caso de que ocurra una excepciï¿½n parseando el texto, se devuelve la cadena vacï¿½a y a correr
+			// no tiene mucho sentido levantar excepciï¿½n por esto
 			logger.error("Ha ocurrido un error parseando un texto html. RARO, RARO... {} - {}", e.getClass().getName(), e.getMessage());
 			return "";
 		}
-		// una vez parseado con éxito, se construye la salida
+		// una vez parseado con ï¿½xito, se construye la salida
 		StringBuilder sb = new StringBuilder(text.length());
 		composeTextForFeeds(fragment, sb);
 		logger.debug("Resultado del parseo de html: {}", sb);
@@ -64,26 +68,34 @@ public final class HTMLUtils {
 	}
 	private static void composeTextForFeeds(Node node, StringBuilder output) {
 		//TODO si un tag a no tiene texto, no se debe imprimir en la salida
-		// se recorre el árbol DOM, hay que comprobar si se trata de algún tag de los aceptados o un texto y escribirlo
+		// se recorre el ï¿½rbol DOM, hay que comprobar si se trata de algï¿½n tag de los aceptados o un texto y escribirlo
 		// si un nodo es de texto, contiene todo el texto que hay entre dos elementos (bien sea el comienzo o fin del tag o un tag nuevo)
 		if (node instanceof HTMLParagraphElementImpl) output.append("<p>");
-		else if (node instanceof HTMLAnchorElementImpl) output.append("<a href=\"").append(((HTMLAnchorElementImpl)node).getHref()).append("\">");
-		else if (node instanceof HTMLImageElementImpl) output.append("<img src=\"").append(((HTMLImageElementImpl) node).getSrc()).append("\"/>");
-		//IMPORTANTE como esto va a un feed que muestra html y nekohtml deshace la codificación, se recodifican las entidades html del texto
-		else if (node instanceof TextImpl) output.append(Reform.HtmlEncode(node.getNodeValue()));
+		else if (node instanceof HTMLAnchorElementImpl) {
+			logger.debug("Nodo de enlace: {}", ((HTMLAnchorElementImpl)node).getHref());
+			output.append("<a href=\"").append(((HTMLAnchorElementImpl)node).getHref()).append("\">");
+		} else if (node instanceof HTMLImageElementImpl) {
+			logger.debug("Nodo de imagen: {}", ((HTMLImageElementImpl) node).getSrc());
+			output.append("<img src=\"").append(((HTMLImageElementImpl) node).getSrc()).append("\"/>");
+		}
+		//IMPORTANTE como esto va a un feed que muestra html y nekohtml deshace la codificaciï¿½n, se recodifican las entidades html del texto
+		else if (node instanceof TextImpl) {
+			logger.debug("Nodo de texto: {}", node.getNodeValue());
+			output.append(Reform.HtmlEncode(node.getNodeValue()));
+		}
 		Node child = node.getFirstChild();
 		while (child != null) {
 			composeTextForFeeds(child, output);
 			child = child.getNextSibling();
 		}
-		// una vez recorrido el árbol en profundidad, se escriben los elementos finales de los tags que lo llevan
+		// una vez recorrido el ï¿½rbol en profundidad, se escriben los elementos finales de los tags que lo llevan
 		if (node instanceof HTMLParagraphElementImpl) output.append("</p>");
 		else if (node instanceof HTMLAnchorElementImpl) output.append("</a>");
 	}
 
 	
 	/**
-	 * Elimina todos los tags de html. Específico para la indexación en lucene.
+	 * Elimina todos los tags de html. Especï¿½fico para la indexaciï¿½n en lucene.
 	 */
 	public static String parseTextForLucene(String text) {
 		logger.debug("Procesando html para indexar en lucene: {}", text);
@@ -98,7 +110,7 @@ public final class HTMLUtils {
 				i += 3;
 			}
 		}
-		logger.debug("Resultado de la inserción de blancos: {}", sb);
+		logger.debug("Resultado de la inserciï¿½n de blancos: {}", sb);
 		// se crean los elementos del parseador de nekohtml
 		ElementRemover remover = new ElementRemover();
 		DOMFragmentParser parser = new DOMFragmentParser();
@@ -112,14 +124,14 @@ public final class HTMLUtils {
 		} catch(Exception e) {
 			throw new RuntimeException("Ha ocurrido un error parseando un texto html. RARO, RARO... " + e.getClass().getName() + " - " + e.getMessage());
 		}
-		// una vez parseado con éxito, se construye la salida
+		// una vez parseado con ï¿½xito, se construye la salida
 		sb.setLength(0);
 		composeTextForLucene(fragment, sb);
 		logger.debug("Resultado del parseo de html: {}", sb);
 		return sb.toString();
 	}
 	private static void composeTextForLucene(Node node, StringBuilder output) {
-		// se recorre el árbol DOM y se recoge únicamente el texto sin ningún tag
+		// se recorre el ï¿½rbol DOM y se recoge ï¿½nicamente el texto sin ningï¿½n tag
 		if (node instanceof TextImpl) output.append(node.getNodeValue());
 		Node child = node.getFirstChild();
 		while (child != null) {
@@ -129,7 +141,7 @@ public final class HTMLUtils {
 	}
 
 	/**
-	 * Codifica las entidades html pero no las etiquetas. Específico para el retorno de pasajes resaltados por la busqueda de lucene.
+	 * Codifica las entidades html pero no las etiquetas. Especï¿½fico para el retorno de pasajes resaltados por la busqueda de lucene.
 	 */
 	public static String parseTextFromHighlightedLucenePassage(String text) {
 		return ESCAPE_HTML4_CHARACTERS.translate(text);
@@ -140,7 +152,7 @@ public final class HTMLUtils {
 	// Helpers
 	
 	/**
-	 * Translator copiado de Commons Lang para codificar sólo los caractéres, no las etiquetas
+	 * Translator copiado de Commons Lang para codificar sï¿½lo los caractï¿½res, no las etiquetas
 	 */
 	public static final CharSequenceTranslator ESCAPE_HTML4_CHARACTERS = 
 			new AggregateTranslator(
@@ -149,7 +161,7 @@ public final class HTMLUtils {
 			);
 
 	/**
-	 * Translator copiado de Commons Lang para decodificar sólo los caractéres, no las etiquetas
+	 * Translator copiado de Commons Lang para decodificar sï¿½lo los caractï¿½res, no las etiquetas
 	 */
 //	public static final CharSequenceTranslator UNESCAPE_HTML4_CHARACTERS = 
 //			new AggregateTranslator(
