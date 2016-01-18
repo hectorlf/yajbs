b1186c9682e77ce80953a23c93363bc467a757cd
hectorlf <hectorlopezfernandez@hotmail.com>
2016-01-18 18:35:10 +0100

    Finish testing Pebble integration

--- Blog/src/main/java/com/hectorlopezfernandez/integration/CustomPebbleServlet.java
+++ Blog/src/main/java/com/hectorlopezfernandez/integration/CustomPebbleServlet.java
@@ -2,13 +2,18 @@ package com.hectorlopezfernandez.integration;
 
 import javax.servlet.ServletConfig;
 
+import com.hectorlopezfernandez.pebblejodatime.JodaExtension;
+import com.hectorlopezfernandez.pebbleshiro.ShiroExtension;
 import com.hectorlopezfernandez.pebblestripes.PebbleServlet;
 import com.mitchellbosecke.pebble.PebbleEngine;
 
 public class CustomPebbleServlet extends PebbleServlet {
 
 	protected void configureEngine(PebbleEngine.Builder builder, ServletConfig servletConfig) {
-		builder.templateCache(null);
+		ShiroExtension se = new ShiroExtension();
+		JodaExtension je = new JodaExtension();
+		builder.extension(se,je);
+//		builder.templateCache(null);
 	}
 
 }
