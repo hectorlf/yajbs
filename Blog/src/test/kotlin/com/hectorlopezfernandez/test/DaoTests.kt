bdf893e083887b71e2637b828393b501842ff006
hectorlf <hectorlopezfernandez@hotmail.com>
2016-02-21 22:17:31 +0100

    Add a basic kotlin test file

---
+++ Blog/src/test/kotlin/com/hectorlopezfernandez/test/DaoTests.kt
@@ -0,0 +1,23 @@
+package com.hectorlopezfernandez.test
+
+import org.junit.BeforeClass
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class DaoTests {
+
+	companion object {
+		@BeforeClass
+		fun setup() {
+	
+		}
+	}
+
+	@Test
+	fun test() {
+		
+	}
+
+}
