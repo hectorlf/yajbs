dea44c50f4173af07af92525dab17f78dfe799fc
hectorlf <hectorlopezfernandez@hotmail.com>
2018-07-25 01:00:53 +0200

    Bump a bunch of dependencies and fix subsequent compile errors

--- Blog/src/main/java/com/hectorlopezfernandez/action/ListTagPostsAction.java
+++ Blog/src/main/java/com/hectorlopezfernandez/action/ListTagPostsAction.java
@@ -3,7 +3,7 @@ package com.hectorlopezfernandez.action;
 import java.util.Collections;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.owasp.reform.Reform;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
